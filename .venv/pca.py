ef6a17b6f2e80e9a61e25967c84d201b2bc0eda4
Tei Kim <68208668+teikimm307@users.noreply.github.com>
2024-11-19 23:42:16 -0500

    Git commit

--- .venv/pca.py
+++ .venv/pca.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy import linalg as LA
+import matplotlib.pyplot as plt
 
 def pca(matrix):
     matrix = np.array(matrix)
-    centered = matrix - np.mean(matrix, axis = 0)
+    centered = matrix - np.mean(matrix, axis = 0, keepdims=True)
+    print(f"This is the centered matrix: \n {centered} \n")
     covariance = np.cov(centered, rowvar = False)
     print(f"This is the covariance matrix: \n {covariance} \n")
     eigenvalues, eigenvectors = LA.eig(covariance)
@@ -23,6 +25,29 @@ def pca(matrix):
     max1_percent = np.round(max1/sum,3)*100
     max2_percent = np.round(max2/sum, 3)*100
     print(f"The two greatest eigenvalues for the covariance matrix are {np.round(max1,2)} ({max1_percent}%) and {np.round(max2,2)} ({max2_percent}%). Their corresponding eigenvectors are your principle components.")
+
+    sorted_eigenvalues = np.argsort(eigenvalues)[::-1]
+    pc1_index = sorted_eigenvalues[0]
+    pc2_index = sorted_eigenvalues[1]
+    xlist = []
+    for i in range(len(centered)):
+        value = 0
+        for j in range(centered.shape[1]):
+           value += centered[i,j]*eigenvectors[j,pc1_index]
+        xlist.append(value)
+
+    ylist = []
+    for i in range(len(centered)):
+        value = 0
+        for j in range(centered.shape[1]):
+           value += centered[i,j]*eigenvectors[j,pc2_index]
+        ylist.append(value)
+
+    plt.scatter(xlist, ylist, color='red', label='Points')
+    plt.xlabel('PCA 1')
+    plt.ylabel('PCA 2')
+    plt.title('PCA Plot')
+    plt.show()
 
 print("Hello! This code can be used for principal component analysis. \n")
 matrix_input = input("Please input a valid matrix, with entries along rows separated by commas and rows separated by spaces (e.x. 1,8 2,3): ")
